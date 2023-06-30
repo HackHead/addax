377d4dd88a424b109e74d5ae429daddb91772271
Andrew <SolomonAndreew@tutanota.com>
2023-06-30 15:11:35 +0300

    style: define theme variables

---
+++ src/components/styled.ts
@@ -0,0 +1,49 @@
+import styled, {createGlobalStyle} from 'styled-components';
+
+// Card, Button, Typography, Modal, TextField, Label, ButtonGroup, Delimiter, Checkbox, Toggle, Select, MenuItem
+
+const theme = {
+    colors: {
+        primary: '#009EF7',
+        secondary: 'E1E3EA',
+        success: '#50CD89',
+        info: '#7239EA',
+        warning: '#FFC700',
+        danger: '#F1416C',
+        dark: '#181C32',
+        bodyColor: '#FFFFFF',
+        bodyColorRGB: '255 255 255',
+        bodyBg: '1e1e2d',
+        bodyBgRGB: '30, 30, 45',
+        emphasisColor: '#F9F9F9',
+        emphasisColorRGB: '249, 249, 249',
+        secondaryColor: 'rgba(255, 255, 255, .075)',
+        secondaryColorRGB: '255, 255, 255',
+        secondaryBg: '#3F4254',
+        secondaryBgRGB: '63, 66, 84',
+        primaryText: '#6ea8fe',
+        secondaryText: '#E1E3EA',
+        successText: '#75B798',
+        infoText: '#6edff6',
+        warningText: '#ffda6a',
+        dangerText: '#ea868f',
+        lightText: '#F9F9F9',
+        darkText: '#E1E3EA',
+        breakpointXS: 0,
+        breakpointSM: '576px',
+        breakpointMD: '768px',
+        breakpointLG: '992px',
+        breakpointXL: '1200px',
+        breakpointXXL: '1400px',
+        borderRadius: '0.475rem',
+        borderRadiusSM: '0.425rem',
+        borderRadiusLG: '0.625rem',
+        borderRadiusXL: '1rem',
+        borderRadius2XL: '2rem',
+
+        boxShadow: '0 0.5rem 1.5rem 0.5rem rgba(0, 0, 0, 0.075);',
+        boxShadowSM: '0 0.1rem 1rem 0.25rem rgba(0, 0, 0, 0.05);',
+        boxShadowLG: '0 1rem 2rem 1rem rgba(0, 0, 0, 0.1);',
+    }
+}
+
